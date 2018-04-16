c601ce1ed35ead8a1abb7fa5dea4a0fd6feb4057
Zena <zena.vilenia@gmail.com>
2018-04-16 12:08:33 +0700

    client

---
+++ client/src/components/Kanban.vue
@@ -0,0 +1,155 @@
+<template>
+  <div id="container">
+    <Navbar></Navbar>
+    <div class="flex-nav-container">
+      <div class="flex-nav-items">
+        <div id="div-back-log">
+          <p>Back-log</p>
+        </div>
+      </div>
+      <div class="flex-nav-items">
+        <div id="div-todo">
+          <p>To-Do</p>
+        </div>
+        <div class="list">
+          <div class="to-do">
+            <div class="to-do-title">
+              <p class="p-title">Title</p>
+            </div>
+            <div class="to-do-detail">
+              <p class="p-todo-detail">Point:</p>
+              <p class="p-todo-detail">Assigned To:</p>
+              <button class="btn btn-primary btn-sm">Show Detail</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex-nav-items">
+        <div id="div-doing">
+          <p>Doing</p>
+        </div>
+      </div>
+      <div class="flex-nav-items">
+        <div id="div-done">
+          <p>Done</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar';
+
+export default {
+  name: 'Login',
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  background-color: #39f;
+}
+
+
+#div-back-log {
+  background-color: #f30;
+  border-radius: 8px;
+  height: 35px;
+  text-align: left;
+  font-size: 16px;
+  color: white;
+  font-family: Helvetica Neue;
+  padding: 7px;
+}
+
+#div-todo {
+  background-color: #f90;
+  border-radius: 8px;
+  height: 35px;
+  text-align: left;
+  font-size: 16px;
+  color: white;
+  font-family: Helvetica Neue;
+  padding: 7px;
+}
+
+#div-doing {
+  background-color: #39f;
+  border-radius: 8px;
+  height: 35px;
+  text-align: left;
+  font-size: 16px;
+  color: white;
+  font-family: Helvetica Neue;
+  padding: 7px;
+}
+
+#div-done {
+  background-color: #0c6;
+  border-radius: 8px;
+  height: 35px;
+  text-align: left;
+  font-size: 16px;
+  color: white;
+  font-family: Helvetica Neue;
+  padding: 7px;
+}
+
+.flex-nav-container {
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  height: auto;
+  max-width: 1200px;
+  margin: auto;
+  margin-bottom: 10px;
+  flex-direction: row;
+}
+
+.flex-nav-items {
+  width: 23%;
+  border: 2px solid #e3e8ee;
+  border-radius: 8px;
+  margin: 7px;
+}
+
+.to-do {
+  border: 1px solid #e3e8ee;
+  margin: 7px;
+  border-radius: 8px;
+}
+
+.to-do-title {
+  border-bottom: 1px solid #e3e8ee;
+  text-align: left;
+  font-family: Helvetica;
+}
+
+.p-title {
+  padding-top: 10px;
+  padding-left: 10px;
+  font-weight: bold;
+}
+
+.to-do-detail {
+  text-align: left;
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-bottom: 10px;
+}
+
+.p-todo-detail {
+  margin: 0px;
+}
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="flex-nav-container">
+    <div class="flex-nav-left">
+      <h1>Kanban</h1>
+    </div>
+    <div class="flex-nav-right">
+      <button class="btn btn-primary btn-block" data-toggle="modal"
+      data-target="#myModal">New Task</button>
+
+      <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title" align="left">New Task</h4>
+            </div>
+            <div class="modal-body">
+              <form class="ivu-form ivu-form-label-right">
+                <div class="form-group">
+                  <label class="form-title">Title</label>
+                  <div class="form-group-content">
+                    <div class="">
+                      <i class="validate"></i>
+                      <input type="text" placeholder="Task Title" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                <div class="form-group">
+                  <label class="form-title">Description</label>
+                  <div class="form-group-content">
+                    <div class="">
+                      <textarea placeholder="Task Short Description" rows="2" class="form-control"></textarea>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="form-title">Point</label>
+                  <div class="form-group-content">
+                    <div class="">
+                      <i class="validate"></i>
+                      <input type="text" placeholder="Task Point" class="form-control">
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="form-title">Assigned To</label>
+                  <div class="form-group-content">
+                    <div class="">
+                      <i class="validate"></i>
+                      <input type="text" placeholder="Assigned To" class="form-control">
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button class="btn btn-primary">Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div style="clear: both;"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  display: block;
+  font-size: 2em;
+  margin-top: 0;
+  margin-bottom: 0;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
+}
+
+.flex-nav-container {
+  margin-top: 0px;
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+}
+
+.flex-nav-left {
+}
+
+.flex-nav-right {
+  margin-left: auto;
+}
+
+.modal-title {
+  align: left;
+}
+
+.form-title {
+  float: left;
+}
+</style>
